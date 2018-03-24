33ff860dd2bbb07d6121916f5e9f3c5d8cd8e50e
Veronika Kovalska <kovalska.ver@gmail.com>
2018-03-24 12:04:48 +0100

    Navbar

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,12 +1,26 @@
 import React, { Component } from 'react';
+import 'react-flexbox-grid/dist/react-flexbox-grid.css';
+import { Grid, Row, Col } from 'react-flexbox-grid';
 import './App.css';
+import Navbar from './Navbar/Navbar';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-
-      </div>
+      <Grid fluid>
+        <Row>
+          <Col xs={12} md={12}>
+            <div className="App">
+              <Col xs={12}>
+                <Row center="xs">
+                  <Navbar />
+                  <div className="line"></div>
+                </Row>
+              </Col>
+            </div>
+          </Col>
+        </Row>
+      </Grid>
     );
   }
 }
---
+++ frontend/src/components/Navbar/Navbar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Navbar.css';
+
+const Navbar = () => {
+    return (
+        <ul>
+            <li><a href="#">ONA</a></li>
+            <li><a href="#">ON</a></li>
+            <li><a href="#">DIVIDED</a></li>
+            <li><a href="#">DZIECKO</a></li>
+            <li><a href="#">HOME</a></li>
+            <li><a href="#">WYPRZEDAŻ</a></li>
+        </ul>
+    )
+}
+
+export default Navbar;
