8ac22af06612b2d9ca61e22aa5346f1f877dbf7c
Maciej Przydatek <maciekprzydatek@gmail.com>
2018-03-24 15:17:03 +0100

    Speech recognition and synthesis is now working and embedded in App component

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -8,12 +8,6 @@ import Issue from './Issue/Issue';
 import io from 'socket.io-client';
 import SpeechRecognition from './SpeechRecognition';
 
-const speechReco = new SpeechRecognition((transcript) => {
-  console.log(`Transkrypt: ${transcript}`);
-});
-
-speechReco.startRecognition();
-
 const socket = io('http://localhost:4000/customer');
 
 socket.on('greeting', (data) => {
@@ -23,13 +17,37 @@ socket.on('greeting', (data) => {
 
 
 class App extends Component {
+  constructor() {
+    super();
+    this.speechReco = new SpeechRecognition(this.handleRecognizedSpeech);
+  }
+
+  componentDidMount() {
+    // this.speechReco.startRecognition();
+    // this.sythesizeSpeech("eloszka", () => {
+    //   console.log("Skończyło gadać");
+    // });
+  }
+
+  sythesizeSpeech = (text, onEndCallback) => {
+    let msg = new SpeechSynthesisUtterance();
+    msg.text = text;
+    msg.lang = 'pl-PL';
+    msg.onend = onEndCallback;
+    window.speechSynthesis.speak(msg);
+  }
+
+  handleRecognizedSpeech = (transcript) => {
+    console.log(`Transkrypt: ${transcript}`);
+  }
+
   render() {
     return (
       <Grid fluid>
         <Row>
           <Col xs={12} md={12}>
             <div className="App">
-              <Issue/>
+              <Issue />
               <Col xs={12}>
                 <Row center="xs">
                   <Navbar />
